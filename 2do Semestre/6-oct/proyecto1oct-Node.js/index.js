b9a15a5bd9df0bc66f53370f0820ef1338b69837
Ayalken <ayalikeshi@gmail.com>
2025-10-06 13:03:20 -0400

    node 6de oct: con post y get thunder

---
+++ 2do Semestre/6-oct/proyecto1oct-Node.js/index.js
@@ -0,0 +1,21 @@
+//const express = require ('express')
+import express from 'express';
+
+const app = express();
+
+app.get('/eliminacion', (req, res) => {
+    res.send('Eliminacion de datos');
+});
+
+app.post('/adicion', (req, res) => {
+    res.send('adiciona de Empleados');
+});
+
+app.get('/actualizacion', (req, res) => {
+    res.send('actualizacion de Empleados');
+});
+
+const puerto = 3001;
+app.listen(puerto, () => {  
+    console.log(`Servidor en http://localhost:${puerto}`);
+});
